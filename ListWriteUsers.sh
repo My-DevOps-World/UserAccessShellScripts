d14d20aec27ebcf041ff766a88d01fdc5659ea70
Ubuntu <ubuntu@ip-172-31-37-175.us-east-2.compute.internal>
2024-05-19 20:33:40 +0000

    Adding write Access Script

---
+++ ListWriteUsers.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+####################
+#Script to find the users with write role for desired Github Repo
+#Author: Ram
+#Version: v1.0
+###################
+
+# GitHub API URL
+API_URL="https://api.github.com"
+
+# GitHub username and personal access token
+USERNAME=$username
+TOKEN=$token
+
+# User and Repository information
+REPO_OWNER=$1
+REPO_NAME=$2
+
+# Function to make a GET request to the GitHub API
+function github_api_get {
+#takes argument from the function list_users_with_read_access
+    local endpoint="$1"
+    local url="${API_URL}/${endpoint}"
+
+    # Send a GET request to the GitHub API with authentication
+    curl -s -u "${USERNAME}:${TOKEN}" "$url"
+}
+
+# Function to list users with read access to the repository
+function list_users_with_read_access {
+    local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
+
+    # Fetch the list of collaborators on the repository
+    collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.role_name == "write") | .login')"
+
+    # Display the list of collaborators with read access
+    if [[ -z "$collaborators" ]]; then
+        echo "No users with read access found for ${REPO_OWNER}/${REPO_NAME}."
+    else
+        echo "Users with read access to ${REPO_OWNER}/${REPO_NAME}:"
+        echo "$collaborators"
+    fi
+}
+
+# Main script
+
+echo "Listing users with write access to ${REPO_OWNER}/${REPO_NAME}..."
+list_users_with_read_access
