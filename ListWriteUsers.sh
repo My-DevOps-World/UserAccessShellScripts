c2df31f2d620a6ba59823b0c31a7bffcdb60c117
Ubuntu <ubuntu@ip-172-31-37-175.us-east-2.compute.internal>
2024-05-19 20:53:07 +0000

    changed the script for admin users

--- ListWriteUsers.sh
+++ ListWriteUsers.sh
@@ -31,18 +31,20 @@ function list_users_with_read_access {
     local endpoint="repos/${REPO_OWNER}/${REPO_NAME}/collaborators"
 
     # Fetch the list of collaborators on the repository
-    collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.role_name == "write") | .login')"
+    #collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.role_name == "write") | .login')"
+    collaborators="$(github_api_get "$endpoint" | jq -r '.[] | select(.role_name == "admin") | .login')"
+
 
     # Display the list of collaborators with read access
     if [[ -z "$collaborators" ]]; then
-        echo "No users with read access found for ${REPO_OWNER}/${REPO_NAME}."
+        echo "No users with admin access found for ${REPO_OWNER}/${REPO_NAME}."
     else
-        echo "Users with read access to ${REPO_OWNER}/${REPO_NAME}:"
+        echo "Users with admin access to ${REPO_OWNER}/${REPO_NAME}:"
         echo "$collaborators"
     fi
 }
 
 # Main script
 
-echo "Listing users with write access to ${REPO_OWNER}/${REPO_NAME}..."
+echo "Listing users with admin access to ${REPO_OWNER}/${REPO_NAME}..."
 list_users_with_read_access
